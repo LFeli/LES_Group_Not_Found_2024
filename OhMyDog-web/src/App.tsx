5f92e8073cea859e02e7e88616ed15b630cc6952
Larissa <88410319+velocirary@users.noreply.github.com>
2024-02-20 20:34:40 -0300

    :wrench: feat: Adicionado config base de Lints (Eslint, Prettier, PrettierTailwind, Eslint-simple-import-sort)

--- OhMyDog-web/src/App.tsx
+++ OhMyDog-web/src/App.tsx
@@ -1,5 +1,9 @@
+import './global.css'
+
 export function App() {
   return (
-    <div>Hello World</div>
+    <div className="h-screen w-screen bg-zinc-950 text-zinc-100 ">
+      <h1 className="text-5xl">Hello World</h1>
+    </div>
   )
 }
