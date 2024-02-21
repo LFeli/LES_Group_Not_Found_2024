b7399c756badf906eaa62a11bc9f126339665cb3
Lucas Araújo <lucasfelipemk@gmail.com>
2024-02-20 22:01:10 -0300

    :truck: chore: Movendo a pasta 'Pages' para fora de 'components'

--- OhMyDog-web/src/routes.tsx
+++ OhMyDog-web/src/routes.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AppLayout } from './components/pages/_layouts/app'
-import { AuthLayout } from './components/pages/_layouts/auth'
-import { Home } from './components/pages/app/home'
-import { SignIn } from './components/pages/auth/sign-in'
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+import { Home } from './pages/app/home'
+import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
