9b4a75e6261468b64ec4f962c618f5f1e2480cb0
Lucas Felipe <lucasfelipemk@gmail.com>
2024-02-27 21:55:57 -0300

    :sparkles: feat: Tela de login com formulário validado via zod e página estilizada

--- OhMyDog-web/src/App.tsx
+++ OhMyDog-web/src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { Toaster } from './components/ui/sonner'
 import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
@@ -12,6 +13,7 @@ export function App() {
     <HelmetProvider>
       <Helmet titleTemplate="%s | OhMyDog." />
 
+      <Toaster richColors />
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
--- OhMyDog-web/src/pages/_layouts/auth.tsx
+++ OhMyDog-web/src/pages/_layouts/auth.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 export function AuthLayout() {
   return (
     <div className="grid min-h-screen grid-cols-2 antialiased">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+      <div className="bg-login flex h-full w-full flex-col justify-between border-r border-foreground/5 bg-green-500 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <PawPrint className="h-5 w-5 " />
           <span className="font-semibold">OhMyDog.</span>
