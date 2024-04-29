90d024cd4a0ffb435b5c016230a9d7a9d6a5b6c2
Lucas Felipe <lucasfelipemk@gmail.com>
2024-04-29 20:18:12 -0300

    :sparkles: feat: reasons for sponser section is done in home app

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 
 import { CtaApp } from '@/components/app/cta'
+import { ReasonsSponsorApp } from '@/components/app/reasons-sponsor-app'
 
 export function Home() {
   return (
@@ -9,6 +10,7 @@ export function Home() {
 
       <div className="container max-w-screen-2xl px-6">
         <CtaApp />
+        <ReasonsSponsorApp />
       </div>
     </main>
   )
