4200d9a7304bf3349aa03e354685facb92c8f0a2
Lucas Felipe <lucasfelipemk@gmail.com>
2024-04-28 23:33:39 -0300

    :sparkles: feat: Created header component for app layout

---
+++ src/components/header-app.tsx
@@ -0,0 +1,33 @@
+import { PawPrint } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
+import { NavLink } from './nav-links'
+import { Button } from './ui/button'
+
+export function HeaderApp() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-yellow-50/95 backdrop-blur supports-[backdrop-filter]:bg-yellow-50/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-between py-12">
+        {/* Logo */}
+        <div className="flex items-center gap-3">
+          <PawPrint className="h-8 w-8 text-yellow-500" />
+          <span className="text-2xl font-semibold">Oh My Dog.</span>
+        </div>
+
+        <nav className="flex items-center space-x-12">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/postagens">Postagens</NavLink>
+          <NavLink to="/faq">FAQ</NavLink>
+          <NavLink to="/patrocinador">Patrocinador</NavLink>
+          <NavLink to="/contato">Contato</NavLink>
+        </nav>
+
+        <Link to="/login">
+          <Button className="rounded-full border-4 border-yellow-200 bg-yellow-500 px-8 py-5 text-base font-medium text-zinc-800 hover:bg-yellow-200">
+            Login
+          </Button>
+        </Link>
+      </div>
+    </header>
+  )
+}
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom'
 
+import { HeaderApp } from '@/components/header-app'
+
 export function AppLayout() {
   return (
-    <div>
-      <span>AppLayout</span>
+    <div className="flex min-h-screen flex-col bg-yellow-50">
+      <HeaderApp />
+
       <Outlet />
+
+      <footer className="bg-zinc-800 py-16">Footer</footer>
     </div>
   )
 }
