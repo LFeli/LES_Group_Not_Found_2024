e6df0bb0c8e8b1d9cfe4d7c8aba944c7b9960b2a
Lucas Felipe <lucasfelipemk@gmail.com>
2024-05-09 21:14:48 -0300

    :truck: chore: Move page files for new structure

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AdminLayout } from '@/pages/_layouts/admin'
 import { AppLayout } from '@/pages/_layouts/app'
 import { AuthLayout } from '@/pages/_layouts/auth'
+import { RegisterLayout } from '@/pages/_layouts/register'
+import { AdminHome } from '@/pages/admin/home'
+import { Contact } from '@/pages/app/contact'
+import { Faq } from '@/pages/app/faq'
 import { Home } from '@/pages/app/home'
+import { Posts } from '@/pages/app/posts'
+import { Sponsor } from '@/pages/app/sponsor'
+import { AuthHome } from '@/pages/auth/home'
 import { SignIn } from '@/pages/register/sign-in'
 import { SignUp } from '@/pages/register/sign-up'
-
-import { AdminLayout } from './pages/_layouts/admin'
-import { RegisterLayout } from './pages/_layouts/register'
-import { AdminHome } from './pages/admin/home'
-import { Contact } from './pages/app/contact'
-import { Faq } from './pages/app/faq'
-import { Posts } from './pages/app/posts'
-import { Sponsor } from './pages/app/sponsor'
-import { AuthHome } from './pages/auth/home'
-import { PrivateRoute } from './private-route'
+import { PrivateRoute } from '@/private-route'
 
 export const router = createBrowserRouter([
   // No-Authenticated User routes
