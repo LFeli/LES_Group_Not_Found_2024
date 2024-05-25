86c28e341450a6938f97536ad9883e8c955f5034
Lucas Felipe <lucasfelipemk@gmail.com>
2024-05-25 16:14:40 -0300

    :recycle: refactor: Redone sign-in-form schema and alter form for working with Auth Context

--- src/pages/register/sign-in/components/form.tsx
+++ src/pages/register/sign-in/components/form.tsx
@@ -1,18 +1,17 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useMutation } from '@tanstack/react-query'
 import { useForm } from 'react-hook-form'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 
-import { postSignIn } from '@/api/post-sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { setCookie } from '@/utils/cookie'
+import { useAuth } from '@/context/auth-context'
 
-import { SignInFormSchema, SignInSchema } from '../schemas/form'
+import { SignInFormSchema, SignInSchema } from '../schemas/sign-in-form-schema'
 
 export function Form() {
+  const { login, isLogin } = useAuth()
   const [searchParams] = useSearchParams()
   const navigate = useNavigate()
 
@@ -27,19 +26,15 @@ export function Form() {
     },
   })
 
-  const { mutateAsync: authenticateFn } = useMutation({
-    mutationFn: postSignIn,
-    onSuccess: (data) => {
-      setCookie({ name: 'omdAuth', value: data })
-    },
-  })
-
   async function onFormSubmit({ email, password }: SignInFormSchema) {
     try {
-      await authenticateFn({ email, password })
+      await login({ email, password })
       navigate('/app/')
+      toast.success('Login efetuado com sucesso!')
     } catch {
-      toast.error('Credenciais inválidas.')
+      toast.error(
+        'Ocorreu um erro ao efetuar o login, por favor tente novamente.',
+      )
     }
   }
 
@@ -52,7 +47,7 @@ export function Form() {
         <Input
           id="email"
           type="email"
-          disabled={isSubmitting}
+          disabled={isSubmitting || isLogin}
           {...register('email')}
         />
 
@@ -70,7 +65,7 @@ export function Form() {
         <Input
           id="password"
           type="password"
-          disabled={isSubmitting}
+          disabled={isSubmitting || isLogin}
           {...register('password')}
         />
 
@@ -81,7 +76,7 @@ export function Form() {
         )}
       </article>
 
-      <Button disabled={isSubmitting} className="w-full">
+      <Button disabled={isSubmitting || isLogin} className="w-full">
         Fazer login
       </Button>
     </form>
--- src/pages/register/sign-in/schemas/form.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const SignInSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
-})
-
-export type SignInFormSchema = z.infer<typeof SignInSchema>
