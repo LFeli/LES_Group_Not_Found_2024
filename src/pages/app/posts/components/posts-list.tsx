445a345c0b0ff3cb33af4db3c3f12cd578a8c19d
Lucas Felipe <lucasfelipemk@gmail.com>
2024-05-28 21:37:54 -0300

    :construction: posts lits in construction

--- src/pages/app/posts/components/posts-list.tsx
+++ src/pages/app/posts/components/posts-list.tsx
@@ -1,11 +1,25 @@
-import { DialogPost } from '@/components/dialog-post'
+import { useQuery } from '@tanstack/react-query'
+
+import { getAllPosts } from '@/api/get-all-posts'
+import { PostCard } from '@/components/post-card'
 
 export function PostsList() {
+  const { data: AllPosts } = useQuery({
+    queryFn: getAllPosts,
+    queryKey: ['post', 'all-posts'],
+  })
+
+  console.log(AllPosts)
+
   return (
     <section className="grid grid-cols-3 gap-10 pb-32">
-      {Array.from({ length: 9 }).map((_, index) => (
+      {/* {Array.from({ length: 9 }).map((_, index) => (
         <DialogPost key={index} />
-      ))}
+      ))} */}
+      {/* {AllPosts?.map((post) => (
+        <PostCard key={post.postID} content={post.content} />
+      ))} */}
+      <span>Posts here...</span>
     </section>
   )
 }
