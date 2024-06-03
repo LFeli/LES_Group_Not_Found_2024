9924a17abc0a04ccd833b6518574d02d286f62fd
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-03 00:44:25 -0300

    :sparkles: feat: Add way to control dropdown-menu + dialog

--- src/pages/auth/components/user-menu.tsx
+++ src/pages/auth/components/user-menu.tsx
@@ -9,10 +9,12 @@ import {
   Plus,
   TicketPercent,
 } from 'lucide-react'
+import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 
 import { Button } from '@/components/ui/button'
+import { Dialog } from '@/components/ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -23,92 +25,115 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { useAuth } from '@/context/auth-context'
 
-export function UserMenu() {
-  const { user, signOut } = useAuth()
+import { UserEditInfosDialog } from './user-edit-infos-dialog'
 
+export function UserMenu() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+  const { user, signOut } = useAuth()
   const navigate = useNavigate()
-  async function handleLogOut() {
-    await signOut()
+
+  function handleLogOut() {
+    signOut()
     navigate('/login', { replace: true })
     toast.success('Logout efetuado com sucesso!')
   }
 
+  function handleOpenDialog() {
+    setIsDropdownOpen(false)
+    setIsDialogOpen(true)
+  }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant={'outline'} className="group space-x-4 bg-transparent">
-          <span className="font-rubik text-base">Olá {user?.name}</span>
+    <>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant={'outline'}
+            className="group space-x-4 bg-transparent"
+          >
+            <span className="font-rubik text-base">Olá {user?.name}</span>
 
-          <ChevronDown className="duration-50000 transition-transform group-data-[state=open]:rotate-180" />
-        </Button>
-      </DropdownMenuTrigger>
+            <ChevronDown className="duration-50000 transition-transform group-data-[state=open]:rotate-180" />
+          </Button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" className="w-[200px] bg-yellow-50">
-        <DropdownMenuLabel>Ações</DropdownMenuLabel>
-        <DropdownMenuSeparator />
+        <DropdownMenuContent align="end" className="w-[200px] bg-yellow-50">
+          <DropdownMenuLabel>Ações</DropdownMenuLabel>
+          <DropdownMenuSeparator />
 
-        <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
-          <Plus className="h-4 w-4" />
-          <span>Criar Postagem</span>
-        </DropdownMenuItem>
-
-        <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
-          <Pencil className="h-4 w-4" />
-          <span>Editar perfil</span>
-        </DropdownMenuItem>
-
-        <DropdownMenuSeparator />
-        <DropdownMenuLabel>Navegação</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-
-        <Link to={'/app/meus-posts'}>
           <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
-            <Files className="h-4 w-4" />
-            <span>Meus Posts</span>
+            <Plus className="h-4 w-4" />
+            <span>Criar Postagem</span>
           </DropdownMenuItem>
-        </Link>
 
-        <Link to={'/app/postagens'}>
-          <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
-            <HandHeart className="h-4 w-4" />
-            <span>Fazer doação</span>
+          <DropdownMenuItem
+            className="cursor-pointer space-x-3 focus:bg-green-100"
+            onClick={handleOpenDialog}
+          >
+            <Pencil className="h-4 w-4" />
+            <span>Editar perfil</span>
           </DropdownMenuItem>
-        </Link>
 
-        <Link to={'/app/doacoes'}>
-          <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
-            <Handshake className="h-4 w-4" />
-            <span>Contribuições</span>
-          </DropdownMenuItem>
-        </Link>
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel>Navegação</DropdownMenuLabel>
+          <DropdownMenuSeparator />
 
-        <Link to={'/app/denuncia'}>
-          <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
-            <Flag className="h-4 w-4" />
-            <span>Denúncias</span>
-          </DropdownMenuItem>
-        </Link>
+          <Link to={'/app/meus-posts'}>
+            <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
+              <Files className="h-4 w-4" />
+              <span>Meus Posts</span>
+            </DropdownMenuItem>
+          </Link>
 
-        <DropdownMenuSeparator />
-        <DropdownMenuLabel>Patrocinador</DropdownMenuLabel>
-        <DropdownMenuSeparator />
+          <Link to={'/app/postagens'}>
+            <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
+              <HandHeart className="h-4 w-4" />
+              <span>Fazer doação</span>
+            </DropdownMenuItem>
+          </Link>
 
-        <Link to={'/app/patrocinador'}>
-          <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
-            <TicketPercent className="h-4 w-4" />
-            <span>Gerenciar vouchers</span>
-          </DropdownMenuItem>
-        </Link>
+          <Link to={'/app/doacoes'}>
+            <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
+              <Handshake className="h-4 w-4" />
+              <span>Contribuições</span>
+            </DropdownMenuItem>
+          </Link>
 
-        <DropdownMenuSeparator />
+          <Link to={'/app/denuncia'}>
+            <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
+              <Flag className="h-4 w-4" />
+              <span>Denúncias</span>
+            </DropdownMenuItem>
+          </Link>
 
-        <span onClick={handleLogOut}>
-          <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel>Patrocinador</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+
+          <Link to={'/app/patrocinador'}>
+            <DropdownMenuItem className="cursor-pointer space-x-3 focus:bg-green-100">
+              <TicketPercent className="h-4 w-4" />
+              <span>Gerenciar vouchers</span>
+            </DropdownMenuItem>
+          </Link>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem
+            className="cursor-pointer space-x-3 focus:bg-green-100"
+            onClick={handleLogOut}
+          >
             <LogOut className="h-4 w-4" />
             <span>Sair</span>
           </DropdownMenuItem>
-        </span>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <UserEditInfosDialog closeDialog={() => setIsDialogOpen(false)} />
+      </Dialog>
+    </>
   )
 }
