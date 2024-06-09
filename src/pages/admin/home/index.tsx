c48a80bca20a9b93b6c3c8259780ca9ef4f74963
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-09 10:14:32 -0300

    :sparkles: feat: Create quick-links on home of admin

--- src/pages/admin/home/index.tsx
+++ src/pages/admin/home/index.tsx
@@ -1,3 +1,15 @@
+import { Helmet } from 'react-helmet-async'
+
+import { AdminQuickLinks } from './components/quick-links'
+
 export function AdminHome() {
-  return <span>Admin home...</span>
+  return (
+    <>
+      <Helmet title="Admin Home" />
+
+      <div className="container max-w-screen-2xl px-6 py-16">
+        <AdminQuickLinks />
+      </div>
+    </>
+  )
 }
