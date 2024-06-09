4245ef8bd76914b8f93ffb9231ae8c58ca73e46f
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-09 10:12:58 -0300

    :sparkles: feat: Create admin base layout

--- src/pages/_layouts/admin.tsx
+++ src/pages/_layouts/admin.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom'
 
+import { Footer } from '../auth/components/footer'
+import { Header } from '../auth/components/header'
+
 export function AdminLayout() {
   return (
-    <>
-      <div>
-        <span>AdminLayout</span>
+    <div className="flex min-h-screen flex-col">
+      <Header />
+
+      <div className="flex-1">
         <Outlet />
       </div>
-    </>
+
+      <Footer />
+    </div>
   )
 }
