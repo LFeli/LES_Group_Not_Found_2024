36d916c69d2b0e0aea4ee9bd976ef2031d15b542
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-12 21:48:40 -0300

    :bug: fix: Remove some things for sponsor not-auth page

--- src/pages/auth/sponsor/components/cta.tsx
+++ src/pages/auth/sponsor/components/cta.tsx
@@ -1,8 +1,3 @@
-import { ChevronRight } from 'lucide-react'
-import { Link } from 'react-router-dom'
-
-import { Button } from '@/components/ui/button'
-
 export function Cta() {
   return (
     <div className="min-h-[940px] bg-accent-foreground bg-[url(../../../../src/assets/app/sponsor/cta.jpg)] bg-cover bg-no-repeat">
@@ -16,23 +11,6 @@ export function Cta() {
           apoiam a causa animal e ajudar animais necessitados, Junte-se a nós e
           faça parte desta jornada.
         </p>
-
-        <div className="space-x-8">
-          <Link to={'/login'}>
-            <Button className="rounded-full bg-jeans-700 px-6 py-6 text-zinc-100 hover:bg-jeans-500">
-              Doar Agora
-            </Button>
-          </Link>
-
-          <Link to={'/'}>
-            <Button
-              variant={'ghost'}
-              className="gap-2 rounded-full px-6 py-6 text-zinc-200 hover:bg-jeans-50"
-            >
-              Saiba Mais <ChevronRight className="h-5 w-5" />
-            </Button>
-          </Link>
-        </div>
       </div>
     </div>
   )
