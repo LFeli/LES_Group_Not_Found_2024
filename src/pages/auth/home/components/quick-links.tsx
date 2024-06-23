e17f4b2b178ac86b56226fdf7dc5c55adca8cd73
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-23 17:47:13 -0300

    :sparkles: feat: Add links in quick-links in auth home

--- src/pages/auth/home/components/quick-links.tsx
+++ src/pages/auth/home/components/quick-links.tsx
@@ -1,15 +1,14 @@
 import { Dialog } from '@radix-ui/react-dialog'
 import { Files, HandHeart, PlusCircle, TicketPercent } from 'lucide-react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import { Button } from '@/components/ui/button'
 
 import { NewPostDialog } from '../../components/new-post-dialog'
-import { NewVoucherDialog } from '../../sponsor/components/new-voucher-dialog'
 
 export function QuickLinks() {
-  const [isNewPostDialog, setIsNewPostDialog] = useState(false)
-  const [isNewVoucherDialog, setIsNewVoucherDialog] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <section className="flex flex-col items-center justify-start space-y-24 pb-32">
@@ -25,7 +24,7 @@ export function QuickLinks() {
         <article className="overflow-hidden rounded-md">
           <Button
             variant={'ghost'}
-            onClick={() => setIsNewPostDialog(true)}
+            onClick={() => setIsOpen(true)}
             className="group h-20 whitespace-normal p-0 hover:bg-green-50"
           >
             <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
@@ -42,113 +41,74 @@ export function QuickLinks() {
         </article>
 
         <article className="rounded-md">
-          <Button
-            variant={'ghost'}
-            className="group h-20 whitespace-normal p-0 hover:bg-green-50"
-          >
-            <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
-              <Files className="h-8 w-8" />
-            </div>
+          <Link to={'/app/meus-posts'}>
+            <Button
+              variant={'ghost'}
+              className="group h-20 whitespace-normal p-0 hover:bg-green-50"
+            >
+              <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
+                <Files className="h-8 w-8" />
+              </div>
 
-            <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
-              <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
-                Ver minhas postagens
-              </h4>
-              <span className="max-w-[260px] text-xs">
-                Veja uma lista de todas as suas postagens de maneira fácil.
+              <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
+                <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
+                  Ver minhas postagens
+                </h4>
+                <span className="max-w-[260px] text-xs">
+                  Veja uma lista de todas as suas postagens de maneira fácil.
+                </span>
               </span>
-            </span>
-          </Button>
+            </Button>
+          </Link>
         </article>
 
         <article className="rounded-md">
-          <Button
-            variant={'ghost'}
-            className="group h-20 whitespace-normal p-0 hover:bg-green-50"
-          >
-            <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
-              <HandHeart className="h-8 w-8" />
-            </div>
+          <Link to={'/app/doacoes'}>
+            <Button
+              variant={'ghost'}
+              className="group h-20 whitespace-normal p-0 hover:bg-green-50"
+            >
+              <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
+                <HandHeart className="h-8 w-8" />
+              </div>
 
-            <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
-              <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
-                Ver doações feitas
-              </h4>
-              <span className="max-w-[260px] text-xs">
-                Veja todas as suas doações e acompanhe o status delas.
+              <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
+                <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
+                  Ver doações feitas
+                </h4>
+                <span className="max-w-[260px] text-xs">
+                  Veja todas as suas doações e acompanhe o status delas.
+                </span>
               </span>
-            </span>
-          </Button>
+            </Button>
+          </Link>
         </article>
 
-        {/* <article className="rounded-md">
-          <Button
-            variant={'ghost'}
-            onClick={() => setIsNewVoucherDialog(true)}
-            className="group h-20 whitespace-normal p-0 hover:bg-green-50"
-          >
-            <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
-              <Send className="h-8 w-8" />
-            </div>
-
-            <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
-              <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
-                Criar voucher
-              </h4>
-              <span className="max-w-[260px] text-xs">
-                Crie um vouncher agora mesmo.
-              </span>
-            </span>
-          </Button>
-        </article> */}
-
-        {/* <article className="rounded-md">
-          <Button
-            variant={'ghost'}
-            className="group h-20 whitespace-normal p-0 hover:bg-green-50"
-          >
-            <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
-              <TicketPercent className="h-8 w-8" />
-            </div>
-
-            <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
-              <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
-                Gerenciar voucher
-              </h4>
-              <span className="max-w-[260px] text-xs">
-                Acesse o painel para gerenciar todos os seus vouchers
-              </span>
-            </span>
-          </Button>
-        </article> */}
-
         <article className="rounded-md">
-          <Button
-            variant={'ghost'}
-            className="group h-20 whitespace-normal p-0 hover:bg-green-50"
-          >
-            <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
-              <TicketPercent className="h-8 w-8" />
-            </div>
+          <Link to={'/app/denuncia'}>
+            <Button
+              variant={'ghost'}
+              className="group h-20 whitespace-normal p-0 hover:bg-green-50"
+            >
+              <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
+                <TicketPercent className="h-8 w-8" />
+              </div>
 
-            <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
-              <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
-                Ver minhas denúncias
-              </h4>
-              <span className="max-w-[260px] text-xs">
-                Veja o andamento das suas denúncias feitas dentro do site.
+              <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
+                <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
+                  Ver minhas denúncias
+                </h4>
+                <span className="max-w-[260px] text-xs">
+                  Veja o andamento das suas denúncias feitas dentro do site.
+                </span>
               </span>
-            </span>
-          </Button>
+            </Button>
+          </Link>
         </article>
       </div>
 
-      <Dialog open={isNewPostDialog} onOpenChange={setIsNewPostDialog}>
-        <NewPostDialog closeDialog={() => setIsNewPostDialog(false)} />
-      </Dialog>
-
-      <Dialog open={isNewVoucherDialog} onOpenChange={setIsNewVoucherDialog}>
-        <NewVoucherDialog closeDialog={() => setIsNewVoucherDialog(false)} />
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <NewPostDialog closeDialog={() => setIsOpen(false)} />
       </Dialog>
     </section>
   )
