3ca9090e69f00209a08e660646739d2d4dee9b02
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-27 06:21:17 -0300

    :sparkles: feat: Created function for download-file

---
+++ src/utils/download-file.ts
@@ -0,0 +1,13 @@
+export async function downloadFile(fileURL: string, fileName: string) {
+  const response = await fetch(fileURL)
+  const blob = await response.blob()
+  const url = window.URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.style.display = 'none'
+  a.href = url
+  a.download = fileName
+  document.body.appendChild(a)
+  a.click()
+  window.URL.revokeObjectURL(url)
+  document.body.removeChild(a)
+}
