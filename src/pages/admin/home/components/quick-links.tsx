6f063a2d263b60b3827bf8a3168f12ac1fcece47
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-27 23:54:06 -0300

    :sparkles: feat: Add links for adm quick menu

--- src/pages/admin/home/components/quick-links.tsx
+++ src/pages/admin/home/components/quick-links.tsx
@@ -1,4 +1,5 @@
 import { AlertTriangle, Files, PiggyBank, Users } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 import { Button } from '@/components/ui/button'
 
@@ -15,88 +16,97 @@ export function AdminQuickLinks() {
       </div>
 
       <div className="grid grid-cols-3 gap-x-[130px] gap-y-12">
-        <article className="overflow-hidden rounded-md">
-          <Button
-            variant={'ghost'}
-            className="group h-20 whitespace-normal p-0 hover:bg-green-50"
-          >
-            <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
-              <Files className="h-8 w-8" />
-            </div>
+        <Link to={'/admin/postagem-aprovacao'}>
+          <article className="overflow-hidden rounded-md">
+            <Button
+              variant={'ghost'}
+              className="group h-20 whitespace-normal p-0 hover:bg-green-50"
+            >
+              <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
+                <Files className="h-8 w-8" />
+              </div>
 
-            <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
-              <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
-                Gerenciar Postagens
-              </h4>
-              <span className="w-[290px] text-xs">
-                Veja uma lista com todas as postagens e as opções para
-                gerenciar.
+              <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
+                <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
+                  Gerenciar Postagens
+                </h4>
+                <span className="w-[290px] text-xs">
+                  Veja uma lista com todas as postagens e as opções para
+                  gerenciar.
+                </span>
               </span>
-            </span>
-          </Button>
-        </article>
+            </Button>
+          </article>
+        </Link>
 
-        <article className="rounded-md">
-          <Button
-            variant={'ghost'}
-            className="group h-20 whitespace-normal p-0 hover:bg-green-50"
-          >
-            <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
-              <PiggyBank className="h-8 w-8" />
-            </div>
+        <Link to={'/admin/patrocinadores-aprovacao'}>
+          <article className="rounded-md">
+            <Button
+              variant={'ghost'}
+              className="group h-20 whitespace-normal p-0 hover:bg-green-50"
+            >
+              <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
+                <PiggyBank className="h-8 w-8" />
+              </div>
 
-            <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
-              <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
-                Gerenciar Patrocinadores
-              </h4>
-              <span className="max-w-[260px] text-xs">
-                Veja uma lista com todos os patrocinadores e as opções para
-                gerenciar.
+              <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
+                <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
+                  Gerenciar Patrocinadores
+                </h4>
+                <span className="max-w-[260px] text-xs">
+                  Veja uma lista com todos os patrocinadores e as opções para
+                  gerenciar.
+                </span>
               </span>
-            </span>
-          </Button>
-        </article>
+            </Button>
+          </article>
+        </Link>
 
-        <article className="rounded-md">
-          <Button
-            variant={'ghost'}
-            className="group h-20 whitespace-normal p-0 hover:bg-green-50"
-          >
-            <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
-              <Users className="h-8 w-8" />
-            </div>
+        <Link to={'/admin/gerenciar-usuarios'}>
+          <article className="rounded-md">
+            <Button
+              variant={'ghost'}
+              className="group h-20 whitespace-normal p-0 hover:bg-green-50"
+            >
+              <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
+                <Users className="h-8 w-8" />
+              </div>
 
-            <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
-              <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
-                Gerenciar Usuários
-              </h4>
-              <span className="max-w-[260px] text-xs">
-                Veja uma lista com todos os usuários e as opções para gerenciar.
+              <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
+                <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
+                  Gerenciar Usuários
+                </h4>
+                <span className="max-w-[260px] text-xs">
+                  Veja uma lista com todos os usuários e as opções para
+                  gerenciar.
+                </span>
               </span>
-            </span>
-          </Button>
-        </article>
+            </Button>
+          </article>
+        </Link>
 
-        <article className="rounded-md">
-          <Button
-            variant={'ghost'}
-            className="group h-20 whitespace-normal p-0 hover:bg-green-50"
-          >
-            <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
-              <AlertTriangle className="h-8 w-8" />
-            </div>
+        <Link to={'/admin/denuncias'}>
+          <article className="rounded-md">
+            <Button
+              variant={'ghost'}
+              className="group h-20 whitespace-normal p-0 hover:bg-green-50"
+            >
+              <div className="flex h-20 w-[80px] items-center justify-center rounded-md bg-green-100">
+                <AlertTriangle className="h-8 w-8" />
+              </div>
 
-            <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
-              <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
-                Gerenciar Denúncias
-              </h4>
-              <span className="max-w-[260px] text-xs">
-                Veja uma lista com todas as denúncias e as opções para
-                gerenciar.
+              <span className="flex h-full flex-col items-start justify-start gap-1 px-4 pt-2 text-left">
+                <h4 className="font-rubik font-semibold group-hover:underline group-hover:underline-offset-2">
+                  Gerenciar Denúncias
+                </h4>
+                <span className="max-w-[260px] text-xs">
+                  Veja uma lista com todas as denúncias e as opções para
+                  gerenciar.
+                </span>
               </span>
-            </span>
-          </Button>
-        </article>
+            </Button>
+          </article>
+        </Link>
       </div>
     </section>
   )
