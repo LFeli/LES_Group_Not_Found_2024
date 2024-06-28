26b69fa7d58e9e30435a08696b234234d09bbc54
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-27 23:05:03 -0300

    :sparkles: feat: Row action, approve, reject and download proof it's done

--- src/pages/auth/user-posts/components/data-table-row-action.tsx
+++ src/pages/auth/user-posts/components/data-table-row-action.tsx
@@ -1,20 +1,20 @@
 import { DotsHorizontalIcon } from '@radix-ui/react-icons'
+import { useMutation } from '@tanstack/react-query'
 import { Row } from '@tanstack/react-table'
+import { Check, CircleSlash2, Download } from 'lucide-react'
 
-// import { Eye } from 'lucide-react'
-// import { useState } from 'react'
+import { patchApproveDonation } from '@/api/patch-approve-donation'
+import fileURL from '@/assets/auth.jpg'
 import { Button } from '@/components/ui/button'
-// import { Dialog } from '@/components/ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
-  // DropdownMenuItem,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import { downloadFile } from '@/utils/download-file'
 
 import { postUserSchema } from '../schemas/post-user-schema'
-
-// import { ApprovalDialog } from './approval-dialog'
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>
@@ -23,7 +23,15 @@ interface DataTableRowActionsProps<TData> {
 export function DataTableRowActions<TData>({
   row,
 }: DataTableRowActionsProps<TData>) {
-  // const [isViewPostDialog, setIsViewPostDialog] = useState(false)
+  const { mutateAsync: patchApproveDonationFn } = useMutation({
+    mutationFn: patchApproveDonation,
+  })
+
+  function handleDownload() {
+    const fileUrl = fileURL
+    const fileName = 'comprovante.png'
+    downloadFile(fileUrl, fileName)
+  }
 
   const link = postUserSchema.parse(row.original)
   console.log('Output of link on DataTableRowActions component: ', link)
@@ -41,12 +49,37 @@ export function DataTableRowActions<TData>({
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="">
-        {/* <DropdownMenuItem
-          onClick={() => setIsViewPostDialog(true)}
+        <DropdownMenuItem
+          onClick={() =>
+            patchApproveDonationFn({
+              donationID: link.donationID,
+              status: 'A',
+            })
+          }
           className="flex items-center justify-start gap-2"
         >
-          <Eye className="h-4 w-4" /> Ver publicação
-        </DropdownMenuItem> */}
+          <Check className="h-4 w-4" /> Aprovar doação
+        </DropdownMenuItem>
+
+        <DropdownMenuItem
+          onClick={() =>
+            patchApproveDonationFn({
+              donationID: link.donationID,
+              status: 'I',
+            })
+          }
+          className="flex items-center justify-start gap-2"
+        >
+          <CircleSlash2 className="h-4 w-4" /> Rejeitar doação
+        </DropdownMenuItem>
+
+        <DropdownMenuItem
+          className="flex items-center justify-start gap-2"
+          onClick={handleDownload}
+        >
+          <Download className="h-4 w-4" />
+          Baixar comprovante
+        </DropdownMenuItem>
       </DropdownMenuContent>
 
       {/* <Dialog open={isViewPostDialog} onOpenChange={setIsViewPostDialog}>
