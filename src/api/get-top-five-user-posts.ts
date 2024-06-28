2bb591e9e0d7c7d391ee183ff51fbb71b8216b99
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-27 22:13:46 -0300

    :sparkles: feat: Create route for return top five user posts

---
+++ src/api/get-top-five-user-posts.ts
@@ -0,0 +1,88 @@
+import { api } from '@/lib/axios'
+
+interface getUserPostsProps {
+  userID: number | undefined
+}
+
+export interface getTopFiveUserPostsResponse {
+  userID: string
+  userDonorID: string
+  postID: string
+  sponsorID: string
+  voucherID: string
+  donationValue: string
+  message: string
+  proofPix: string
+  donationType: string
+  donatedAt: string
+  donorStatus: string
+  voucherName: string
+  donorName: string
+  titlePost: string
+  statusPost: string
+}
+
+interface apiResponse {
+  idDoacao: string
+  idUsuarioDoador: string
+  idPostagem: string
+  idPatrocinador: string
+  idVoucher: string
+  valorDoacao: string
+  mensagem: string
+  comprovantePix: string
+  idTipoDoacao: string
+  dtDoacao: string
+  status: string
+  cupomVoucher: string
+  nomeUsuarioDoador: string
+  tituloPostagem: string
+  statusPostagem: string
+}
+
+const postStatusLabels: { [key: string]: string } = {
+  P: 'Em Análise',
+  I: 'Desativada',
+  A: 'Ativo',
+}
+
+const donorStatusLabels: { [key: string]: string } = {
+  P: 'Em Análise',
+  A: 'Aceito',
+}
+
+function convertApiResponse(
+  data: apiResponse | apiResponse[],
+): getTopFiveUserPostsResponse[] {
+  const dataArray = Array.isArray(data) ? data : [data]
+
+  return dataArray.map((item) => ({
+    userID: item.idDoacao,
+    userDonorID: item.idUsuarioDoador,
+    postID: item.idPostagem,
+    sponsorID: item.idPatrocinador,
+    voucherID: item.idVoucher,
+    donationValue: item.valorDoacao,
+    message: item.mensagem,
+    proofPix: item.comprovantePix,
+    donationType: item.idTipoDoacao,
+    donatedAt: item.dtDoacao,
+    donorStatus: donorStatusLabels[item.status],
+    voucherName: item.cupomVoucher,
+    donorName: item.nomeUsuarioDoador,
+    titlePost: item.tituloPostagem,
+    statusPost: postStatusLabels[item.statusPostagem],
+  }))
+}
+
+export async function getTopFiveUserPosts({
+  userID,
+}: getUserPostsProps): Promise<getTopFiveUserPostsResponse[]> {
+  const response = await api.get<apiResponse[]>(
+    `/Doacao/UltimasDoacoesUsuarioSolicitante/${userID}`, // change route after
+  )
+
+  const data: getTopFiveUserPostsResponse[] = convertApiResponse(response.data)
+
+  return data
+}
