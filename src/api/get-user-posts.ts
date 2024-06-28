af6cff15ad49253e7573b8063b44c4c1925a20bc
Lucas Felipe <lucasfelipemk@gmail.com>
2024-06-27 22:13:15 -0300

    :bug: fix: Update currrect values on get-user-posts

--- src/api/get-user-posts.ts
+++ src/api/get-user-posts.ts
@@ -4,37 +4,51 @@ interface getUserPostsProps {
   userID: number | undefined
 }
 
-interface postContent {
-  title: string
-  description: string
-  publishedAt: string
-  approvedAt: string
-  userID: string
-  administratorID: string
-  imagem: string
-  donationType: string
-  pixKey: string
-  status: string
-  donationGoal: number
-  donationsRaised: number
-}
-
 export interface getUserPostsResponse {
+  userID: string
+  userDonorID: string
   postID: string
-  content: postContent
+  sponsorID: string
+  voucherID: string
+  donationValue: string
+  message: string
+  proofPix: string
+  donationType: string
+  donatedAt: string
+  donorStatus: string
+  voucherName: string
+  donorName: string
+  titlePost: string
+  statusPost: string
 }
 
 interface apiResponse {
+  idDoacao: string
+  idUsuarioDoador: string
   idPostagem: string
-  titulo: string
-  conteudo: string
-  dtPublicacao: string
-  dtAprovacao: string
-  idUsuario: string
-  idAdminstrador: string
-  imagem: string
-  chavePix: string
+  idVoucher: string
+  valorDoacao: string
+  mensagem: string
+  comprovantePix: string
+  idTipoDoacao: string
+  dtDoacao: string
   status: string
+  cupomVoucher: string
+  nomeUsuarioDoador: string
+  tituloPostagem: string
+  statusPostagem: string
+  idPatrocinador: string
+}
+
+const postStatusLabels: { [key: string]: string } = {
+  P: 'Em Análise',
+  I: 'Desativada',
+  A: 'Ativo',
+}
+
+const donorStatusLabels: { [key: string]: string } = {
+  P: 'Em Análise',
+  A: 'Aceito',
 }
 
 function convertApiResponse(
@@ -43,21 +57,21 @@ function convertApiResponse(
   const dataArray = Array.isArray(data) ? data : [data]
 
   return dataArray.map((item) => ({
+    userID: item.idDoacao,
+    userDonorID: item.idUsuarioDoador,
     postID: item.idPostagem,
-    content: {
-      title: item.titulo,
-      description: item.conteudo,
-      publishedAt: item.dtPublicacao,
-      approvedAt: item.dtAprovacao,
-      userID: item.idUsuario,
-      administratorID: item.idAdminstrador,
-      imagem: item.imagem,
-      donationType: 'Remédio',
-      pixKey: item.chavePix,
-      status: item.status,
-      donationGoal: 520,
-      donationsRaised: 1000,
-    },
+    sponsorID: item.idVoucher,
+    voucherID: item.valorDoacao,
+    donationValue: item.mensagem,
+    message: item.comprovantePix,
+    proofPix: item.idTipoDoacao,
+    donationType: item.dtDoacao,
+    donatedAt: item.status,
+    donorStatus: donorStatusLabels[item.cupomVoucher],
+    voucherName: item.nomeUsuarioDoador,
+    donorName: item.tituloPostagem,
+    titlePost: item.statusPostagem,
+    statusPost: postStatusLabels[item.idPatrocinador],
   }))
 }
 
@@ -65,7 +79,7 @@ export async function getUserPosts({
   userID,
 }: getUserPostsProps): Promise<getUserPostsResponse[]> {
   const response = await api.get<apiResponse[]>(
-    `/Doacao/DoacaoUsuario/${userID}`, // change route after
+    `/Doacao/TodasDoacoesUsuarioSolicitante/${userID}`, // change route after
   )
 
   const data: getUserPostsResponse[] = convertApiResponse(response.data)
